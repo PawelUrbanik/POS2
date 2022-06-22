1ff291ead099dec8176f22bc10aa0ec8282ee59b
Pawel <pawelurbanik94@gmail.com>
2022-06-22 15:51:07 +0200

    Add Edge DTO Read model.
    See: #5

---
+++ src/main/java/pl/pawel/model/edge/DTO/EdgeReadModel.java
@@ -0,0 +1,46 @@
+package pl.pawel.model.edge.DTO;
+
+import pl.pawel.model.edge.Edge;
+import pl.pawel.model.track.Track;
+
+import java.util.Set;
+
+public class EdgeReadModel {
+
+    private String edgeName;
+    private Set<String> tracks;
+    private EdgePlatformReadModel platform;
+
+
+    public EdgeReadModel(Edge edge) {
+        this.edgeName = edge.getEgdeName();
+        edge.getTracks().stream()
+                .map(Track::getTrackNumber)
+                .forEach(trackName -> tracks.add(String.valueOf(trackName)));
+        platform = new EdgePlatformReadModel(edge.getPlatform());
+    }
+
+    public String getEdgeName() {
+        return edgeName;
+    }
+
+    public void setEdgeName(String edgeName) {
+        this.edgeName = edgeName;
+    }
+
+    public Set<String> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(Set<String> tracks) {
+        this.tracks = tracks;
+    }
+
+    public EdgePlatformReadModel getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(EdgePlatformReadModel platform) {
+        this.platform = platform;
+    }
+}
