8ffd40f3a1bf86258f499ba92ca6777f0ad22289
Pawel <pawelurbanik94@gmail.com>
2022-10-13 20:48:48 +0200

    Add Discriminant frontend table See #20

--- Frontend/angular-client/src/app/app.module.ts
+++ Frontend/angular-client/src/app/app.module.ts
@@ -7,6 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DiscriminantListComponent } from './component/discriminant-list/discriminant-list.component';
 import { HttpClientModule } from '@angular/common/http'
 import { DiscriminantService } from './services/discriminant.service';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +26,12 @@ import { DiscriminantService } from './services/discriminant.service';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [DiscriminantService],
   bootstrap: [AppComponent]
--- Frontend/angular-client/src/app/component/discriminant-list/discriminant-list.component.ts
+++ Frontend/angular-client/src/app/component/discriminant-list/discriminant-list.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Discriminant } from 'src/app/common/discriminant';
 import { DiscriminantService } from 'src/app/services/discriminant.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-discriminant-list',
   templateUrl: './discriminant-list.component.html',
   styleUrls: ['./discriminant-list.component.css']
 })
-export class DiscriminantListComponent implements OnInit {
+export class DiscriminantListComponent implements OnInit  {
 
   constructor(private discriminantService: DiscriminantService) { }
 
-  discriminants: Discriminant[] = [];
+  dataSource = new MatTableDataSource<Discriminant>();
+  displayedColumns: string[] = ['id', 'shortcut', 'description'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
     this.discriminantService.getDiscriminant().subscribe(data =>{
-        this.discriminants = data;
+        this.dataSource = new MatTableDataSource<Discriminant>(data);
+        this.dataSource.paginator = this.paginator;
     });
-
-    console.log(this.discriminants);
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 
 
 }
