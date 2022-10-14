6fcf1721f1246931ce2564e8499b0ffbc8b9a82d
Pawel <pawelurbanik94@gmail.com>
2022-10-14 20:58:56 +0200

    Add lombok See #11

--- src/main/java/pl/pawel/track/model/Track.java
+++ src/main/java/pl/pawel/track/model/Track.java
@@ -1,5 +1,7 @@
 package pl.pawel.track.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
 import pl.pawel.model.edge.Edge;
 
@@ -7,82 +9,17 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Track {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private Integer trackNumber;
     private Double axisKm;
     @ManyToOne
     @Nullable
     @JoinColumn(name = "edge_id", nullable = true)
     private Edge edge;
-
-    public Track(Long id, Integer trackNumber, Double axisKm, Edge edge) {
-        this.id = id;
-        this.trackNumber = trackNumber;
-        this.axisKm = axisKm;
-        this.edge = edge;
-    }
-
-    public Track() {
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getTrackNumber() {
-        return trackNumber;
-    }
-
-    public void setTrackNumber(Integer trackNumber) {
-        this.trackNumber = trackNumber;
-    }
-
-    public Double getAxisKm() {
-        return axisKm;
-    }
-
-    public void setAxisKm(Double axisKm) {
-        this.axisKm = axisKm;
-    }
-
-    public Edge getEdge() {
-        return edge;
-    }
-
-    public void setEdge(Edge edge) {
-        this.edge = edge;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Track track = (Track) o;
-        return Objects.equals(id, track.id) && Objects.equals(trackNumber, track.trackNumber) && Objects.equals(axisKm, track.axisKm) && Objects.equals(edge, track.edge);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, trackNumber, axisKm, edge);
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", trackNumber=" + trackNumber +
-                ", axisKm=" + axisKm +
-                ", edge=" + edge.getEgdeName() +
-                '}';
-    }
 }
--- src/main/java/pl/pawel/track/model/dto/TrackWriteDto.java
+++ src/main/java/pl/pawel/track/model/dto/TrackWriteDto.java
@@ -1,33 +1,13 @@
 package pl.pawel.track.model.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class TrackWriteDto {
 
     private Integer trackNumber;
     private Double axisKm;
-
     private Long EdgeId;
-
-    public Integer getTrackNumber() {
-        return trackNumber;
-    }
-
-    public void setTrackNumber(Integer trackNumber) {
-        this.trackNumber = trackNumber;
-    }
-
-    public Double getAxisKm() {
-        return axisKm;
-    }
-
-    public void setAxisKm(Double axisKm) {
-        this.axisKm = axisKm;
-    }
-
-    public Long getEdgeId() {
-        return EdgeId;
-    }
-
-    public void setEdgeId(Long edgeId) {
-        EdgeId = edgeId;
-    }
 }
