aa5e24e8bb711c5ff2a19e873177a10dfdbc471d
Pawel <pawel.urbanik-dev@gmail.com>
2024-11-20 20:59:42 +0100

    POS-12 Backend: Fixed number of edges count

--- src/main/java/pl/pawel/platform/mapper/PlatformMapper.java
+++ src/main/java/pl/pawel/platform/mapper/PlatformMapper.java
@@ -1,15 +1,24 @@
 package pl.pawel.platform.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import pl.pawel.edge.model.Edge;
 import pl.pawel.platform.model.Platform;
 import pl.pawel.platform.model.PlatformSelectOptionDto;
 import pl.pawel.platform.model.PlatformTabTableRowDto;
+
+import java.util.Set;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PlatformMapper {
 
     PlatformSelectOptionDto entityToDto(Platform platform);
 
+    @Mapping(target = "numberOfEdges", expression = "java(countEdges(platform.getEdges()))")
     PlatformTabTableRowDto entityToTableRowDto(Platform platform);
+
+    default Integer countEdges(Set<Edge> edges) {
+        return edges == null ? 0 : edges.size();
+    }
 }
